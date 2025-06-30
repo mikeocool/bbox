3ccdfd9eccce265f4b022419507f62d2ab0ea204
Mike O'Toole <mike.otoole@gmail.com>
2025-06-30 15:19:40 +0000

    ssh ssh setup

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,8 @@
 
 set -eou pipefail
 
+# fix ssh dir permissions
+sudo chown ${USER}:${USER} ~/.ssh
+
 sudo sed -i 's/^#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
 npm install -g @anthropic-ai/claude-code
-
-python -m pip install -U --upgrade-strategy only-if-needed aider-chat[browser]
